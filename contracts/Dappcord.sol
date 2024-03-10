f8e897780b37293309874f0d18eb3ac4afe5d2dc
tzafff <christzaf99@gmail.com>
2024-03-10 15:22:35 +0200

    Fetch BC data and add navbar

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
-
-    address public owner;
-    uint256 public totalChannels;
     uint256 public totalSupply;
+    uint256 public totalChannels;
+    address public owner;
 
     struct Channel {
         uint256 id;
@@ -15,40 +14,42 @@ contract Dappcord is ERC721 {
         uint256 cost;
     }
 
-    mapping (uint256 => Channel) public channels;
-    mapping (uint256 => mapping(address => bool)) public hasJoined;
+    mapping(uint256 => Channel) public channels;
+    mapping(uint256 => mapping(address => bool)) public hasJoined;
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
+        require(msg.sender == owner);
         _;
     }
 
-    constructor(string memory _name, string memory _symbol) 
-        ERC721(_name, _symbol)  {
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
         owner = msg.sender;
     }
 
-    function createChannel(string memory _name, uint256 _cost) public onlyOwner{
+    function createChannel(string memory _name, uint256 _cost)
+        public
+        onlyOwner
+    {
         totalChannels++;
-        channels[totalChannels] = Channel(1, _name, _cost);
+        channels[totalChannels] = Channel(totalChannels, _name, _cost);
     }
 
-    function getChannel(uint256 _id) public view returns(Channel memory) {
-        return channels[_id];
-    }
-
-    function mint(uint256 _id) public payable{
-
+    function mint(uint256 _id) public payable {
         require(_id != 0);
         require(_id <= totalChannels);
         require(hasJoined[_id][msg.sender] == false);
         require(msg.value >= channels[_id].cost);
-        // Join Channel
-        hasJoined[_id][msg.sender] = true;
 
-        // Mint NFT
+        hasJoined[_id][msg.sender] = true;
         totalSupply++;
+
         _safeMint(msg.sender, totalSupply);
+    }
+
+    function getChannel(uint256 _id) public view returns (Channel memory) {
+        return channels[_id];
     }
 
     function withdraw() public onlyOwner {
--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import { io } from "socket.io-client"
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { io } from "socket.io-client";
 
 // Components
-import Navigation from './components/Navigation'
-import Servers from './components/Servers'
-import Channels from './components/Channels'
-import Messages from './components/Messages'
+import Navigation from "./components/Navigation";
+import Servers from "./components/Servers";
+import Channels from "./components/Channels";
+import Messages from "./components/Messages";
 
 // ABIs
-import Dappcord from './abis/Dappcord.json'
+import Dappcord from "./abis/Dappcord.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 // Socket
-const socket = io('ws://localhost:3030');
+const socket = io("ws://localhost:3030");
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [dappcord, setDappcord] = useState(null);
+  const [channels, setChannels] = useState([]);
+
+  const loadBlockchainData = async () => {
+    console.log("loading...");
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    const dappcord = new ethers.Contract(config[network.chainId].Dappcord.address, Dappcord, provider)
+    setDappcord(dappcord)
+
+    const totalChannels = await dappcord.totalChannels()
+    
+    const channels = []
+
+    for(let i = 1; i <= totalChannels; i++) {
+      const channel = await dappcord.getChannel(i);
+      channels.push(channel)
+    }
+    setChannels(channels)
+
+    console.log(channels)
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+
+    window.ethereum.on("accountsChanged", (accounts) => {
+      const newAccount = ethers.utils.getAddress(accounts[0]);
+      setAccount(newAccount);
+    });
+  }, []);
 
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
+      <Navigation account={account} setAccount={setAccount} />
 
       <main>
-
+        <Servers />
+        <Channels />
+        <Messages />
       </main>
     </div>
   );
