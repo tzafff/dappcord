121a323844beaadbd15613b5e2932b909c6393c4
tzafff <christzaf99@gmail.com>
2024-03-12 00:27:30 +0200

    remove

--- src/App.js
+++ src/App.js
@@ -72,10 +72,8 @@ function App() {
       console.log(messages)
       setMessages(messages)
     })
+
     
-    io.engine.on("connection_error", (err) => {
-      console.log(err);
-    });
 
     return () => {
       socket.off('connect')
